161a4a644698186fb01e26768ee2db985d434c1e
Gradyn Wursten <me@gradyn.com>
2024-08-01 14:06:05 -0600

    changes

---
+++ Options.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+
+namespace SpectroTune;
+
+class Options
+{
+    [Option('d', "directory", HelpText = "One or more directories to recursively fix")]
+    public IEnumerable<string> Directories { get; set; }
+    
+    [Option('f', "file", HelpText = "One or more files to fix")]
+    public IEnumerable<string> Files { get; set; }
+    
+    [Option("ffmpeg", Default = "ffmpeg", HelpText = "Path to ffmpeg. Consults global PATH by default")]
+    public string FfmpegPath { get; set; }
+    
+    [Option("ffprobe", Default = "ffprobe", HelpText = "Path to ffprobe. Consults global PATH by default")]
+    public string FfprobePath { get; set; }
+    
+    [Option('t', "threads", HelpText = "Sets the maximum degree of parallelism. Defaults to the number of threads on your system", Default = 0)] // Defaults to 0 since properties have to be compile-time constants. 0 will be interpreted as Environment.ProcessorCount
+    public int Threads { get; set; }
+    
+    [Option("db", HelpText = "Sets the target level in Decibels. Increase to increase volume, decrease to decrease clipping. This will default to -1 which is a good target in most use cases, so you probably won't need to mess with this", Default = -1)]
+    public float decibelTarget { get; set; }
+    
+    [Option("--stereo", HelpText = "Adds a volume-corrected downmixed stereo channel to surround-only files. This is disabled by default as you would not intuitively expect the script to do this, but enabling it is reccomended", Default = false)]
+    public bool stereo { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CommandLine;
-
-using CommandLine;
+using SpectroTune;
 
 var interpretedParams = Parser.Default.ParseArguments<Options>(args);
 
@@ -15,20 +14,17 @@ if (!files.Any())
     return;
 }
 
-class Options
+var maxDegreeParallelism = interpretedParams.Value.Threads == 0 ? Environment.ProcessorCount : interpretedParams.Value.Threads;
+
+Console.WriteLine($"Processing {files.Count()} files on {maxDegreeParallelism} threads");
+
+
+Parallel.ForEach(files, new ParallelOptions() {MaxDegreeOfParallelism = maxDegreeParallelism}, file =>
 {
-    [Option('d', "directory", HelpText = "One or more directories to recursively fix")]
-    public IEnumerable<string> Directories { get; set; }
     
-    [Option('f', "file", HelpText = "One or more files to fix")]
-    public IEnumerable<string> Files { get; set; }
+});
+
+string ExecuteCommand(string path, string[] cmdArgs)
+{
     
-    [Option("--ffmpeg", Default = "ffmpeg", HelpText = "Path to ffmpeg. Consults global PATH by default")]
-    public string FfmpegPath { get; set; }
-    
-    [Option("--ffprobe", Default = "ffprobe", HelpText = "Path to ffprobe. Consults global PATH by default")]
-    public string FfprobePath { get; set; }
-    
-    [Option('t', "threads", HelpText = "Sets the maximum degree of parallelism. Defaults to the number of threads on your system", Default = 0)] // Defaults to 0 since properties have to be compile-time constants. 0 will be interpreted as Environment.ProcessorCount
-    public bool Threads { get; set; }
 }
